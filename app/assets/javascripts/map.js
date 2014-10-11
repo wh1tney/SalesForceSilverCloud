1349e5cda4ce8935bcca364af9af18b721cd8289
Whitney O'Banner <wobanner@gmail.com>
2014-10-11 12:27:00 -0700

    Fullscreen mobile map with geolocation
    * Set map view automatically based on user's current location

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,15 +1,13 @@
 // initialize the map on the "map" div with a given center and zoom
 $(function() {
-  var map = L.map('map', {
-      center: [51.505, -0.09],
-      zoom: 13
-  });
+  var map = L.map('map');
 
   L.tileLayer('https://{s}.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png', {
       attribution: 'Map data',
       maxZoom: 18
   }).addTo(map);
 
+  map.locate({setView: true, maxZoom: 16});
 });
 
 
