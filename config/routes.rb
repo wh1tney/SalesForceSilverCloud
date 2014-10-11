e6c62d653abb1c8bb858d6ee8f8ca3a7611b339c
Matthew Darin <mgd2233@gmail.com>
2014-10-11 01:18:27 -0700

    added route for testing API

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-
+  get '/map' => 'test_forces#index'
   root 'users#login'
 
   # Example of regular route:
