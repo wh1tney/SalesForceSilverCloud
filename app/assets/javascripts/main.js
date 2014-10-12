c0d8bad5c8e55bde4eee0ff16470a7914d20c4e7
narwhaldo <narwhaldo@gmail.com>
2014-10-11 21:47:48 -0700

    modifies index and adds drawer functionality"

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,10 +2,12 @@ $(function(){
 	$('.signup').on('click', function(event){
 		$('.signup_form').addClass('active');
 		$('.signin_form').removeClass('active');
-	})
+	});
 
 	$('.signin').on('click', function(event){
 		$('.signup_form').removeClass('active');
 		$('.signin_form').addClass('active');
-	})
+	});
+
+	$('.menu-link').bigSlide();
 })
