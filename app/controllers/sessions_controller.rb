1a87693cc24d203bfe7e58d0da478abd2f31eff7
Justin Cano <jcano001@ucr.edu>
2014-10-12 07:29:07 -0700

    add chatter posts to objects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,9 @@ class SessionsController < ApplicationController
   include Databasedotcom::Rails::Controller
   def index
     @clients = Client__c.all()
+    client = Databasedotcom::Client.new("config/databasedotcom.yml")
+    client.authenticate :username=>"thesilvercloud2014@gmail.com", :password=>"Hackathon2014Brf4sVX9U09ooty81FYIECIM", :version=>"23.0"
+    @posts = Databasedotcom::Chatter::NewsFeed.find(client)
     #@clients = Client.all()
     #@clients = SFDC_Models::Client.all()
     respond_to do |format|
