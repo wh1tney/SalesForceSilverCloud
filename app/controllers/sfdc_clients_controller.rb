9e2396340b4ec17c40125b6d76dc0201536828b5
Jourdan Bul-lalayao <jbullalayao@auditfile.com>
2014-10-11 19:28:28 -0700

    Allowed user to click on a client through index page and view profile on show page

--- app/controllers/sfdc_clients_controller.rb
+++ app/controllers/sfdc_clients_controller.rb
@@ -2,8 +2,8 @@ class SfdcClientsController < ApplicationController
   include Databasedotcom::Rails::Controller
 
   def index
-    #@clients = Client__c.all()
-    @clients = Client.all()
+    @clients = Client__c.all()
+    #@clients = Client.all()
     #@clients = SFDC_Models::Client.all()
     respond_to do |format|
       format.html
@@ -12,9 +12,7 @@ class SfdcClientsController < ApplicationController
   end
 
   def show
-
-    # CURRENTLY ONLY GETS FIRST ENTRY
-    @client = Client__c.all()[0]
+    @client = Client__c.query("id__c = '#{params[:id]}'").first
 
     @name      = @client.Name
     @address   = @client.address__c
